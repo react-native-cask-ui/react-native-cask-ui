0877f54769b797d217f987482b87725fcc898676
azurechen <azure517981@gmail.com>
2025-01-13 02:38:17 +0800

    Upgrade separator to support both horizontal and vertical.

--- packages/core/src/components/others/Separator.tsx
+++ packages/core/src/components/others/Separator.tsx
@@ -1,22 +1,61 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+
 import { useOverride, useMemoStyles } from '@react-native-cask-ui/theme';
 
 const defaultStyles = StyleSheet.create({
   root: {
+    flexGrow: 1,
     backgroundColor: '#ddd',
+    width: StyleSheet.hairlineWidth,
     height: StyleSheet.hairlineWidth,
+  },
+  label: {
+    color: 'white',
+    paddingHorizontal: 8,
+  },
+});
+
+const fixedStyles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  column: {
+    flexDirection: 'column',
+    alignItems: 'center',
   },
 });
 
 export type SeparatorProps = {
   variant?: string;
+  label?: string;
+  vertical?: boolean;
 };
 
 export default React.memo<SeparatorProps>(props => {
-  const { styles } = useOverride('Separator', props);
+  const { props: overridedProps, styles } = useOverride('Separator', props);
+  const { label, vertical } = overridedProps;
 
-  const finalStyle = useMemoStyles([defaultStyles.root, styles.root]);
+  const finalStyle = useMemoStyles([
+    defaultStyles.root,
+    styles.root,
+    vertical ? { height: undefined } : { width: undefined },
+  ]);
+  const finalLabelStyle = useMemoStyles([defaultStyles.label, styles.label]);
 
-  return <View style={finalStyle} />;
+  if (label) {
+    return (
+      <View style={vertical ? fixedStyles.column : fixedStyles.row}>
+        <View style={finalStyle} />
+        <Text style={finalLabelStyle}>{label}</Text>
+        <View style={finalStyle} />
+      </View>
+    );
+  }
+  return (
+    <View style={vertical ? fixedStyles.column : fixedStyles.row}>
+      <View style={finalStyle} />
+    </View>
+  );
 });
